2fb2d49451c757d41b31fbb86011a38be792b348
marat75 <marat75@gmail.com>
2016-07-03 03:52:27 -0700

    Initial commit

---
+++ app/assets/javascripts/customer_app.js
@@ -0,0 +1,79 @@
+var app = angular.module(
+  'customers',
+  [
+    'ngRoute',
+    'templates'
+  ]
+); 
+
+app.config([
+          "$routeProvider",
+  function($routeProvider) {
+    $routeProvider.when("/", {
+       controller: "CustomerSearchController",
+      templateUrl: "customer_search.html"
+    }).when("/:id",{
+       controller: "CustomerDetailController",
+      templateUrl: "customer_detail.html"
+    });
+  }
+]);
+
+app.controller("CustomerSearchController", [ 
+          '$scope','$http','$location',
+  function($scope , $http , $location) {                         
+
+    var page = 0;
+
+    $scope.customers = [];
+    $scope.search = function(searchTerm) {   
+      $scope.loading = true;
+      if (searchTerm.length < 3) {
+        return;
+      }
+      $http.get("/customers.json",  
+                { "params": { "keywords": searchTerm, "page": page } }
+      ).then(function(response) {
+          $scope.customers = response.data;
+          $scope.loading = false;
+      },function(response) {
+          $scope.loading = false;
+          alert("There was a problem: " + response.status);
+        }
+      );
+    }
+
+    $scope.previousPage = function() {
+      if (page > 0) {
+        page = page - 1;
+        $scope.search($scope.keywords);
+      }
+    }
+    $scope.nextPage = function() {
+      page = page + 1;
+      $scope.search($scope.keywords);
+    }
+
+    $scope.viewDetails = function(customer) {
+      $location.path("/" + customer.id);
+    }
+  }
+]);
+
+app.controller("CustomerDetailController", [ 
+          "$scope","$http","$routeParams",
+  function($scope , $http , $routeParams) {
+
+    var customerId = $routeParams.id;
+    $scope.customer = {};
+
+    $http.get(
+      "/customers/" + customerId + ".json"
+    ).then(function(response) {
+        $scope.customer = response.data;
+      },function(response) {
+        alert("There was a problem: " + response.status);
+      }
+    );
+  }
+]);
